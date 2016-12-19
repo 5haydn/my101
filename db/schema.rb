1cca7f24233ae9c5a7f7e110270b7330daeac524
lipeng <63911226@qq.com>
2016-12-19 15:38:58 +0800

    add post button

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161218165643) do
+ActiveRecord::Schema.define(version: 20161219070516) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
